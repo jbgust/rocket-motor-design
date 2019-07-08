651b55c800c02a06c6a8b63b8100ee396d14cf0d
jbgust <jeromebise@hotmail.com>
2019-07-08 17:33:16 +0200

    Metric METEOR (prometheus/grafana)

---
+++ src/main/java/com/rocketmotordesign/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.rocketmotordesign;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static java.util.Arrays.asList;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${cors.allowed.domains}")
+    private String[] allowedCORSDomains;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/compute")
+                .permitAll()
+//                .requestMatchers(EndpointRequest.to(PrometheusScrapeEndpoint.class))
+//                .authenticated()
+//                .and()
+//                .formLogin()
+                .and()
+                .csrf().disable()
+        ;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(asList(allowedCORSDomains));
+        configuration.setAllowedMethods(asList("*"));
+        configuration.setAllowedHeaders(asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/compute", configuration);
+        return source;
+    }
+}
