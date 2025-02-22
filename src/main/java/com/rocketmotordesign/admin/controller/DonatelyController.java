66a4e11be045858a05d2f4fbe8cbf962470f4f45
Jerome Bise <jeromebise@hotmail.com>
2025-02-22 13:51:11 +0100

    Cache donately responses

--- src/main/java/com/rocketmotordesign/admin/controller/DonatelyController.java
+++ src/main/java/com/rocketmotordesign/admin/controller/DonatelyController.java
@@ -1,18 +1,21 @@
 package com.rocketmotordesign.admin.controller;
 
 import com.rocketmotordesign.admin.controller.response.DonatelyDonationResponse;
-import com.rocketmotordesign.admin.service.DonationResponse;
 import com.rocketmotordesign.admin.service.DonatelyService;
+import com.rocketmotordesign.admin.service.DonationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
+import java.util.*;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -20,7 +23,10 @@ import static org.springframework.http.ResponseEntity.ok;
 @RequestMapping("donations")
 public class DonatelyController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger("donations");
     private final DonatelyService donatelyService;
+    private static Map<LocalDate, DonationResponse> cachedResponses = new HashMap<>();
+
 
     public DonatelyController(DonatelyService donatelyService) {
         this.donatelyService = donatelyService;
@@ -30,10 +36,18 @@ public class DonatelyController {
     @GetMapping
     public ResponseEntity getDonations() {
 
+        LocalDateTime now = LocalDateTime.now();
+        return ok(Optional.ofNullable(cachedResponses.get(now.toLocalDate()))
+                .orElseGet(() -> getDonationResponse(now)));
+    }
+
+    private DonationResponse getDonationResponse(LocalDateTime now) {
+        cachedResponses.clear();
+        LOGGER.info("clear donately response cache");
+
         List<DonatelyDonationResponse> donatelyDonationResponses = donatelyService.retrieveDonations();
         DonationResponse donationResponse = new DonationResponse();
 
-        LocalDateTime now = LocalDateTime.now();
 
         donatelyDonationResponses.forEach(response ->  {
             LocalDateTime donationDate = Instant.ofEpochMilli(response.getDonationDateTimeStamp()).atZone(ZoneId.systemDefault()).toLocalDateTime();
@@ -45,8 +59,10 @@ public class DonatelyController {
                 donationResponse.addRollingMonthDonations(response.getDonationInCent());
             }
         });
+        cachedResponses.put(now.toLocalDate(), donationResponse);
+        LOGGER.info("add donately response to cache");
 
-        return ok(donationResponse);
+        return donationResponse;
     }
 
 }
